cde8862b18966f727b923c703f072143921a10ae
sree-chikati <srechi453.fusdk12@gmail.com>
2020-10-21 19:26:04 -0700

    Required 1: Your Favorite Animal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
 
+#TUTORIAL 1: HELLO WORLD!
 from flask import Flask #Import the Flask library
 
 app = Flask(__name__) #Set up our app variable so that we can start writing routes.
@@ -11,6 +12,18 @@ def homepage():
 #def homepage():, defines the route function. Whatever this function returns will show up in your browser when you visit the appropriate URL
     """Shows a greeting to the user.""" #docstring, and describes the route in a human-readable way
     return 'Are you there, world? It\'s me, Ducky!' #returns the page contents
+
+
+
+#REQUIRED 1: YOUR FAVORITE ANIMAL
+@app.route('/penguins')
+def penguins_page():
+    return "Penguins are cute!"
+
+@app.route('/dogs')
+def dogs_page():
+    return "Dogs are adorable!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
